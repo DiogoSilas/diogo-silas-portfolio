ae6c8c62da4ae3562e9e9af1107189580ee19c87
Diogo Silas <diogosilas4@gmail.com>
2024-01-18 19:20:48 -0300

    GitHub Page não suporta execução de scripts do lado do servidor.

--- assets/scripts/php/index.php
+++ assets/scripts/php/index.php
@@ -1,3 +1,8 @@
+<!-- 
+O problema pode estar relacionado ao fato de que o GitHub Pages não suporta execução de scripts do lado do servidor, incluindo o envio de e-mails através da função mail() do PHP.
+
+O GitHub Pages é um serviço de hospedagem estática, o que significa que ele serve apenas arquivos HTML, CSS, e JavaScript estáticos, sem suporte para processamento de scripts do lado do servidor.
+-->
 <?php
     $username = addslashes($_POST['username']);
     $email = addslashes($_POST['email']);
@@ -14,5 +19,5 @@
         echo("E-mail enviado com sucesso!");
     } else {
         echo("Houve um erro ao enviar o email");
-    };
+    }
 ?>
