d2b3d86981ca22845760e0f1cd49077fed99b3eb
Diogo Silas <diogosilas4@gmail.com>
2024-02-12 09:48:34 -0300

    Cartões da seção de projetos auterados; Responsividade nos cartões da seção "Skills".

--- src/scripts/javascript/index.js
+++ src/scripts/javascript/index.js
@@ -1,7 +1,6 @@
 const btnOpenMenu = document.getElementById('btn-open-menu');
 const overlayMenu = document.getElementById('overlay-menu-mobile');
 const menuMobile = document.getElementById('menu-mobile');
-const btnOpenModal = document.getElementById('btn-open-modal');
 
 const toggleMenu = () => {
     menuMobile.classList.toggle('open-menu');
@@ -10,15 +9,3 @@ const toggleMenu = () => {
 btnOpenMenu.addEventListener('click', toggleMenu);
 overlayMenu.addEventListener('click', toggleMenu);
 menuMobile.addEventListener('click', toggleMenu);
-
-btnOpenModal.addEventListener('click', ()=> {
-    const modal = document.querySelector('.modal-window');
-    const btnCloseModal = document.getElementsByClassName('btn-close-modal');
-    modal.classList.add('open');
-
-    modal.addEventListener('click', (e) => {
-        if(e.target == btnCloseModal[0] || e.target.id == 'modal-window' ) {
-            modal.classList.remove('open');
-        };
-    });
-});
