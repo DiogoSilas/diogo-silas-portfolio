d8bd52f2d0f20709a4c21e77fbc4e220d511c62f
Diogo Silas <diogosilas4@gmail.com>
2024-02-22 18:54:39 -0300

    Adicionado uma seção de habilidades e remoção do texto de digitação automatica.

--- src/scripts/javascript/index.js
+++ src/scripts/javascript/index.js
@@ -2,8 +2,15 @@ const btnOpenMenu = document.getElementById('btn-open-menu');
 const overlayMenu = document.getElementById('overlay-menu-mobile');
 const menuMobile = document.getElementById('menu-mobile');
 
+/**
+ * Toggles the 'open-menu' class on the menuMobile element
+ */
 const toggleMenu = () => {
-    menuMobile.classList.toggle('open-menu');
+    if (menuMobile) {
+        menuMobile.classList.toggle('open-menu');
+    } else {
+        console.error('menuMobile element not found');
+    }
 };
 
 btnOpenMenu.addEventListener('click', toggleMenu);
